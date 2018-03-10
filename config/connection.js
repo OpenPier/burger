747450ef5d5b34990b4fb2a2124ecf64e57568d7
OpenPier <35052947+OpenPier@users.noreply.github.com>
2018-03-10 14:15:01 -0600

    2.0.1

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,20 @@
 
 var mysql = require('mysql');
 //db is local on localhost
-var connection = mysql.createConnection({
-    port: 3306,
+
+var connection;
+
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else  {
+    connection = mysql.createConnection({
     host:'localhost',
     user: 'root',
     password: '',
     database: 'burger_db'
-});
+    });
+};
+
 //This will connect to MySQL
 connection.connect(function (err){
     if (err) {
